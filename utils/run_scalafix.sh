173d2297721263f44b67ef1ff09834f0c11a5a49
Borja Lorente <blorente@ucm.es>
2017-10-12 13:28:47 +0200

    Add useful information

--- utils/run_scalafix.sh
+++ utils/run_scalafix.sh
@@ -6,14 +6,16 @@ if [ "$1" == "-h" ]; then
   echo "Usage: `./utils/$0 [--report \<report folder\>]` - Run scalafix in a given directory"
   exit 0
 elif [ "$1" == "--report" ]; then
-  echo "[Run Scalafix] Reporting to $2"
+  echo "[Run Scalafix] Running Scalafix on $(  pwd )"
   echo "[Run Scalafix] Running rule: $RULE_NAME"
   echo "[Run Scalafix] Running command: $COMMAND"
+  echo "[Run Scalafix] Reporting to $2/$RULE_NAME.txt"
   time sbt -mem 2048 "scalafix $RULE" > "$2/$RULE_NAME.txt"
 else
-  echo "[Run Scalafix] Reporting to console"
+  echo "[Run Scalafix] Running Scalafix on $(  pwd )"
   echo "[Run Scalafix] Running rule: $RULE_NAME"
   echo "[Run Scalafix] Running command: $COMMAND"
-  time sbt -mem 2048 "scalafix $RULE" > "$2/$RULE_NAME.txt"
+  echo "[Run Scalafix] Reporting to console"
+  time sbt -mem 2048 "scalafix $RULE"
 fi
 
